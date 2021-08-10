93a48943b83e17578b6c69ed693a644f4832d75d
alecBeccaria <80131757+alecBeccaria@users.noreply.github.com>
2021-08-10 00:01:44 -0600

    Updates

---
+++ src/Controllers/tempMain.java
@@ -0,0 +1,10 @@
+package Controllers;
+
+import Drivers.Playlist;
+
+public class tempMain {
+    public static void main(String[] args) {
+        Playlist p = new Playlist();
+        p.createNewPlaylist("Playlist_2");
+    }
+}
--- src/Drivers/Playlist.java
+++ src/Drivers/Playlist.java
@@ -4,13 +4,11 @@ import java.io.File;
 
 public class Playlist {
     readWrite rw = new readWrite();
-    File[] playList = null;
+    File playList = null;
 
     public void createNewPlaylist(String playlistName) {
        File newPlayList = new File("Playlists\\" + playlistName);
-       if (!newPlayList.isDirectory()){
-           newPlayList.mkdir();
-       }
-
+       newPlayList.mkdirs();
+       newPlayList = playList;
     }
 }
