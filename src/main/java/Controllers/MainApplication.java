869c2e6ce916f5f69352b33a66cafe080b4b41ba
alecBeccaria <80131757+alecBeccaria@users.noreply.github.com>
2021-08-18 21:32:40 -0600

    Attempt to use new GUI

--- src/main/java/Controllers/MainApplication.java
+++ src/main/java/Controllers/MainApplication.java
@@ -14,8 +14,8 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 600);
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("newGUI.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 800);
         stage.setTitle("The Best Java Music Player");
         stage.setScene(scene);
         stage.show();
